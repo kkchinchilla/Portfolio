0c565536a7a63092c5f9e76568d327570384ed20
Karen Chang <chang.karen@me.com>
2023-04-18 22:53:28 -0700

    scroll

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+// Scroll Animation
+
+const boxes =  document.querySelectorAll('.work__container')
+
+document.body.addEventListener('scroll', checkBoxes)
+
+// checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight/ 3.5 * 3;
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top 
+
+        if(boxTop < triggerBottom ){
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
